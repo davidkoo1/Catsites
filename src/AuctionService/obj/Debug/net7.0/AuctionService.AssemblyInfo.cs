ee851e791a9f200eb3a87177b1c82ece544fb4c5
davidk001 <sverceks@gmail.com>
2024-01-16 13:08:46 +0200

    SearchService dockerMongoDB

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7c573183348d9daa9cd28780d4fb1a9804c74d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040eea57f032174b51310c9823ed3ce64a49e5da")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
