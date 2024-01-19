04dd6621eaa9b58d86f0878af98ca8227b340fe9
davidk001 <sverceks@gmail.com>
2024-01-19 10:00:34 +0200

    Adding Contracts

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8927b3a06133f70f5ecebd4d76f5ce3947cdb7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b9edd1d26cb4ddec39fcc8e67eea23abb05813")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
