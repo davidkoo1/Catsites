792298ca2e521033ba3b6183d8d42045121ff0f0
davidk001 <sverceks@gmail.com>
2024-01-19 15:12:54 +0200

    Using message retries | create_error --need fix

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9ca1a60ec5c9f5d286c48dd1dcfaf58e0ad923")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b7281d0c94d7b6b65ba92b773c9b908d771ec0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,3 @@
-using Contracts;
 using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
@@ -12,29 +11,30 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
-
 builder.Services.AddMassTransit(x =>
 {
-
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
 
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint("search-auction-created", e =>
+        {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
 
-
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-
+// Configure the HTTP request pipeline.
 app.UseAuthorization();
 
 app.MapControllers();
@@ -51,13 +51,10 @@ app.Lifetime.ApplicationStarted.Register(async () =>
     }
 });
 
-
-
-
 app.Run();
 
 static IAsyncPolicy<HttpResponseMessage> GetPolicy()
     => HttpPolicyExtensions
-    .HandleTransientHttpError()
-    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
-    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
