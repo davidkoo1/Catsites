6bc71342b7a6b57691478a331e60775de1456bdb
davidk001 <sverceks@gmail.com>
2024-01-24 16:09:37 +0200

    Changes after fail Testing gateway, nowGood

--- src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net7.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69b90764da42019f8c49b4c7225be9dcfc3b539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33ec50686f78803fe745d0df44368fb235a2384")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -2,8 +2,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
@@ -17,9 +17,5 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 var app = builder.Build();
 
 app.MapReverseProxy();
-
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.Run();
