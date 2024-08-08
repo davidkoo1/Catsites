41032bd225a33b6e1711d19530b223934e4a4f99
davidkoo1 <sverceks@gmail.com>
2024-08-08 20:12:28 +0300

    Fetching data from the API

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,11 +1,11 @@
 import { Console } from "console";
 import { Container } from "postcss";
+import Listings from "./auctions/Listings";
 
 export default function Home() {
-  console.log('Server component');
   return (
     <div>
-      Main container
+      <Listings />
     </div>
    
   );
