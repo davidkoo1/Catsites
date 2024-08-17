9293fdf14ff7411cc2041c1e098bb3163f3cbc3d
davidkoo1 <sverceks@gmail.com>
2024-08-17 21:49:17 +0300

    Adding the login functionality

---
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import DuendeIdentityServer6 from "next-auth/providers/duende-identity-server6";
+
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    providers: [
+        DuendeIdentityServer6({
+            id: 'id-server',
+            clientId: 'nextApp',
+            clientSecret: 'secret',
+            issuer: 'http://localhost:5000',
+            authorization: {params: {scope: 'openid profile auctionApp'}},
+            idToken: true
+        })
+    ]
+}
+
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST}
--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -10,14 +10,14 @@
 import React from 'react';
 import Search from './Search';
 import Logo from './Logo';
+import LoginButton from './LoginButton';
 
 export default function Navbar() {
   return (
     <header className='sticky top-0 z-50 flex justify-between bg-black p-4 items-center text-gray-300 shadow-xl'>
       <Logo />
       <Search />
-
-      <button className='bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded'>Login</button>
+      <LoginButton />
     </header>
   );
 }
